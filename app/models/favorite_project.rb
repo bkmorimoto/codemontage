6bc1ea1567c2cc20760db7b21df0e5824f7feb2e
Ben Morimoto <bkmorimoto@gmail.com>
2015-06-24 15:29:44 -0400

    add new scopes to track active favorites

--- app/models/favorite_project.rb
+++ app/models/favorite_project.rb
@@ -4,6 +4,9 @@ class FavoriteProject < ActiveRecord::Base
 
   validates_uniqueness_of :project_id, :scope => :user_id
 
+  scope :active, -> { where(removed: false) }
+  scope :removed, -> { where(removed: true) }
+
   def self.created_between(start_date, end_date)
     where("created_at >= ? AND created_at <= ?", start_date, end_date)
   end
